1ed650899d52542fb8b938372b606d03e42c9d70
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-07-02 13:35:29 +0200

    DOC: update docstring

--- Adhesion/ReferenceSolutions/JKR.py
+++ Adhesion/ReferenceSolutions/JKR.py
@@ -23,38 +23,76 @@
 #
 
 """
-Helper tools for PyCo
+
+JKR solutions for the indentation of an elastic halfspace by a paraboloid
+indenter.
+
+The parameters of the system are:
+
+radius : float, optional
+    Sphere (actually paraboloid) radius.
+contact_modulus : float, optional
+    Contact modulus: :math:`E^* = E/(1-\nu**2)`
+    with Young's modulus E and Poisson number :math:`\nu`.
+    The default value is so that Maugis's contact Modulus is one
+    (:math:`K = 4 / 3 E^*`)
+work_of_adhesion : float, optional
+    Work of adhesion.
+
+This module provides implementations of the formulas relating the rigid body
+penetration (`penetration`), the indentation force (`force`) and the radius
+of the contact disk (`contact_radius`).
+
+If the parameters are not provided, the relations are nondimensional.
+
+The nondimensionalisation follows
+Maugis's book (p.290):
+
+    # TODO: put the Latex formulas of the nondimensionalisation here
+
 """
 
 import math
 
 import numpy as np
 
+
 ###
 
-def radius(N, R, Es, w):
-    """
+def contact_radius(force,
+                   radius=1.,
+                   contact_modulus=3. / 4,
+                   work_of_adhesion=1 / np.pi):
+    r"""
     Given normal load, sphere radius and contact modulus compute contact radius
     and peak pressure.
 
+    if only force is provided, it is assumed that the nondimensionalisation
+    from Maugis's book is used.
+
     Parameters
     ----------
-    N : float
+    force : float
         Normal force.
-    R : float
-        Sphere radius.
-    Es : float
-        Contact modulus: Es = E/(1-nu**2) with Young's modulus E and Poisson
-        number nu.
-    w : float
+    radius : float, optional
+        Sphere (actually paraboloid) radius.
+    contact_modulus : float, optional
+        Contact modulus: :math:`E^* = E/(1-\nu**2)`
+        with Young's modulus E and Poisson number :math:`\nu`.
+        The default value is so that Maugis's contact Modulus is one
+        (:math:`K = 4 / 3 E^*`)
+    work_of_adhesion : float, optional
         Work of adhesion.
     """
-    A = N + 3*w*math.pi*R
-    B = np.sqrt(6*w*math.pi*R*N + (3*w*math.pi*R)**2)
+    A = force + 3 * work_of_adhesion * math.pi * radius
+    B = np.sqrt(6 * work_of_adhesion * math.pi * radius * force + (
+                3 * work_of_adhesion * math.pi * radius) ** 2)
 
-    fac = 3.*R/(4.*Es)
+    fac = 3. * radius / (4. * contact_modulus)
     A *= fac
     B *= fac
 
-    return (A+B)**(1./3), (A-B)**(1./3)
+    return (A + B) ** (1. / 3), (A - B) ** (1. / 3)
+
+
 
