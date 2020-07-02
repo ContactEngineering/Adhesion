BUG: correct bug in second derivative of the stress intensity factor.      remove weird factor pi in the energy release rate.

Adhesion/ReferenceSolutions/JKR.py
@@ -308,8 +308,8 @@ def nonequilibrium_elastic_energy(penetration, contact_radius):
 def nonequilibrium_elastic_energy_release_rate(penetration, contact_radius):
     r"""
 
-    Returns the nondimensional energy release rate (with respect to the nondimensional area) times pi  # TODO: this is odd
-    ..math pi  \frac{\partial U_{el}}{\partial pi A^2} = \frac{3}{8} w_{ref} \frac{1}{A} (\Delta - A^2)^2
+    Returns the nondimensional energy release rate (with respect to the nondimensional area)
+    ..math \frac{\partial U_{el}}{\partial pi A^2} = \frac{3}{8 \pi} w_{ref} \frac{1}{A} (\Delta - A^2)^2
 
     be careful, this is
 
@@ -326,11 +326,11 @@ def nonequilibrium_elastic_energy_release_rate(penetration, contact_radius):
     -------
 
     """
-    return 3 / 8 * (penetration - contact_radius**2)**2 / contact_radius
+    return 3 / 8 / np.pi * (penetration - contact_radius**2)**2 / contact_radius
 
 
 def stress_intensity_factor(contact_radius, penetration, der="0",
-                            radius=1, contact_modulus=1): # TODO: correct contact modulus
+                            radius=1, contact_modulus=3./4):
     r"""
 
     if R is not given, the length and the penetration
@@ -340,10 +340,10 @@ def stress_intensity_factor(contact_radius, penetration, der="0",
     -----------
     a: contact radius
     d: penetration
-    der: {"0", "1_a"}
+    der: {"0", "1_a", "2_a"}
     R: default 1, optional
     radius of the sphere
-    Es: default 1, optional
+    Es: default 3/4, optional
     johnson's contact modulus
 
     Returns:
@@ -351,7 +351,7 @@ def stress_intensity_factor(contact_radius, penetration, der="0",
     stress intensity factor K or it's first derivative according to the
     contact_radius
 
-    if R and Es are not given it is in units of Es \sqrt{R} or Es / \sqrt{R}
+    if R and Es are not given it is in units of 4 / 3 Es \sqrt{R} / R^{der}
     """
     a = contact_radius
     R = radius
@@ -366,9 +366,7 @@ def stress_intensity_factor(contact_radius, penetration, der="0",
     elif der == "1_a":
         return 1 / 2 * a ** (-3/2) * Es * dc / np.sqrt(np.pi) + Es / np.sqrt(np.pi * a) * 2 * a / R
     elif der == "2_a":
-        return - 3 / 4 * a**(-5/2) * Es * dc / np.sqrt(np.pi) \
-               + 1 / 2 * a ** (-3/2) * Es * 2 * a / R / np.sqrt(np.pi) \
-               + Es / np.sqrt(np.pi * a ) / R
+        return - 3 / 4 * a**(-5/2) * Es * dc / np.sqrt(np.pi)
 
 
 def dispField(r, contact_radius, radius, contact_modulus, work_of_adhesion):

tests/ReferenceSolutions/test_sphere_jkr.py
@@ -1,9 +1,98 @@
+import pytest
 
 from Adhesion.ReferenceSolutions import JKR
+
+import numpy as np
 
 
 def test_penetration_radius_inverse():
     params = dict(radius=1., contact_modulus=1., work_of_adhesion=1.)
     assert abs(JKR.contact_radius(
         penetration=JKR.penetration(contact_radius=1., **params), **params) - 1
-        ) < 1e-10
+               ) < 1e-10
+
+
+def test_stress_intensity_factor_energy_release_rate():
+    # tests that the implementation of the strss intensity factor and the
+    # implementation of the energy release rate are equivalent
+
+    e = 3 / 4  # contact modulus
+    a = np.linspace(0.5, 2)  # contact radius
+    penetration = - 0.5
+    en_release_rate = JKR.nonequilibrium_elastic_energy_release_rate(
+        penetration=penetration,
+        contact_radius=a)
+    stress_intensity_factor = JKR.stress_intensity_factor(
+        penetration=penetration,
+        contact_radius=a
+        )
+
+    if False:
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.plot(a, stress_intensity_factor ** 2 / (2 * e), "+",
+                label="$K^2 / (2 E^*)$")
+        ax.plot(a, en_release_rate, "x", label="energy release rate")
+        plt.show()
+
+    np.testing.assert_allclose(stress_intensity_factor ** 2 / (2 * e),
+                               en_release_rate,
+                               atol=1e-14, rtol=1e-10)
+
+
+@pytest.mark.parametrize("radius", (0.1, 1.))
+@pytest.mark.parametrize("contact_modulus", (3 / 4, 1.))
+@pytest.mark.parametrize("penetration", (0.1, 1.))
+def test_stress_intensity_factor_hertzian(radius, contact_modulus,
+                                          penetration):
+    hertzian_radius = np.sqrt(radius * penetration)
+    assert abs(
+        JKR.stress_intensity_factor(contact_radius=hertzian_radius,
+                                    penetration=penetration,
+                                    radius=radius,
+                                    contact_modulus=contact_modulus)) < 1e-15
+
+
+def test_stress_intensity_factor_derivative():
+    a = np.linspace(0.5, 1, 50)
+
+    pen = 0.5
+    am = (a[1:] + a[:-1]) / 2
+    da = a[1:] - a[:-1]
+    dK_da_num = (JKR.stress_intensity_factor(a[1:], pen,)
+                 - JKR.stress_intensity_factor(a[:-1], pen,)) / da
+    dK_da_analytical = JKR.stress_intensity_factor(contact_radius=am,
+            penetration=pen, der="1_a")
+    if False:
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.plot(am, dK_da_num, "+", label="numerical")
+        ax.plot(am, dK_da_analytical, "o", mfc="none",
+            label="analytical")
+        plt.show()
+
+    np.testing.assert_allclose(dK_da_analytical, dK_da_num,
+        atol=1e-14, rtol=1e-4)
+
+
+def test_stress_intensity_factor_second_derivative():
+    a = np.linspace(0.5, 1, 1000)
+
+    pen = 0.5
+    am = a[1:-1]
+    da = a[1] - a[0]
+    dK_da2_num = (JKR.stress_intensity_factor(a[2:], pen,)
+                 - 2 * JKR.stress_intensity_factor(a[1:-1], pen,)
+                 + JKR.stress_intensity_factor(a[:-2], pen,)) / da ** 2
+    dK_da2_analytical = JKR.stress_intensity_factor(contact_radius=am,
+            penetration=pen, der="2_a")
+    if True:
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.plot(am, dK_da2_num, "+", label="numerical")
+        ax.plot(am, dK_da2_analytical, "o", mfc="none",
+            label="analytical")
+        plt.show()
+
+    np.testing.assert_allclose(dK_da2_analytical, dK_da2_num,
+        atol=1e-6, rtol=1e-4)