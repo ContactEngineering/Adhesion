e7dcbf48641f79cf5c9e8c326f9413ba466e16b0
GitHub Action <action@github.com>
2023-05-27 11:48:33 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.91.0.dev44+gc680691',
+    VERSION: '0.91.0.dev46+g57cb1bce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
